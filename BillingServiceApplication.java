4f159df5775a5bacc9e8b057cc8ab5b76e427edd
Gopi Krishna <125242180+gopi7321@users.noreply.github.com>
2025-09-09 09:30:35 +0530

    Add files via upload

---
+++ BillingServiceApplication.java
@@ -0,0 +1,5 @@
+package com.pizzeria.billing;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@SpringBootApplication @EnableFeignClients
+public class BillingServiceApplication { public static void main(String[] args){ SpringApplication.run(BillingServiceApplication.class,args); } }
